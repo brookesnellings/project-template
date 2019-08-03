02267a982172f4dee22febfb7c308ce1bb4bd8a9
Brooke <brooke.snellings@gmail.com>
2019-08-03 12:36:43 -0500

    Add Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,12 @@ const path = require('path');
 
 module.exports = {
   entry: './client/index.js',
+
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+
   module: {
     rules: [
       {
